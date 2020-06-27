cd298d4ddf20166f58a61625ceab69f930ed63cd
XiaoxingChen <chen-xx@zju.edu.cn>
2020-06-28 00:14:43 +0800

    Modify: android root

--- build_android.sh
+++ build_android.sh
@@ -1,10 +1,11 @@
 WORK_DIR=`pwd`
 BUILD_DIR=build
-ANDROID_DIR=/data/local/tmp
+ADB_ROOT=/data/local/tmp
+ANDROID_DIR=$ADB_ROOT/usr/bin/
 
 mkdir -p build
 cd $BUILD_DIR
 cmake .. -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake
 make -j4
-
+adb shell "mkdir -p $ANDROID_DIR"
 adb push vi $ANDROID_DIR
